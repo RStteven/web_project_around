e0f4606025b553d163edfb137d2c0e25b7073092
Steven <ronaldsbs3@gmail.com>
2025-03-04 21:27:08 +0100

    interacción modal

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tripleten web_project_around
+
+Está página web mostrara la una red soacial donde se podra subir imagenes de lo que más le agrade al usuario.
+
+Comenzamos utilizando GitHub para la creación de algunas carpetas y archivos que utilizaremos.
+Con Figma nos podemos ayudar para la creación de la página. Construimos la estructura de la página con HTML, le pusimos estilos con CSS, utilizamos media querys para hacer responsiva la página, y por ultimo, utilizamos JavaScript para hacerla interactiva.
+
+https://rstteven.github.io/web_project_around/
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 let profileModal = document.querySelector(".profile__modal");
-/*let profileModalContent = document.querySelector(".profile__modal-content");
-let profileModalForm = document.querySelector(".profile__modal-form");*/
+/*let profileModalContent = document.querySelector(".profile__modal-content");*/
+
+let profileModalForm = document.querySelector(".profile__modal-form");
 let profileModalCerrar = document.querySelector(".profile__modal-cerrar");
 let profileInfoButton = document.querySelector(".profile__info-button");
 let profileModalOpen = document.querySelector(".profile__modal-open");
+let profileModalInput = document.querySelectorAll(".profile__modal-input");
+let profileInputName = document.querySelector(".profile__input-name");
+let profileName = document.querySelector(".profile__name");
+let profileText = document.querySelector(".profile__text");
+let profileGuardarButton = document.querySelector(".profile__guardar-button");
 
-profileInfoButton.addEventListener("click", function () {
+function handleOpenModal() {
   profileModal.classList.add("profile__modal-open");
-});
+}
 
-profileModalCerrar.addEventListener("click", function () {
+function handleCloseModal() {
+  profileModal.classList.remove("profile__modal-open");
+}
+
+profileInfoButton.addEventListener("click", handleOpenModal);
+profileModalCerrar.addEventListener("click", handleCloseModal);
+
+profileModalForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log(profileModalInput);
+  profileName.textContent = profileModalInput[0].value;
+  profileText.textContent = profileModalInput[1].value;
   profileModal.classList.remove("profile__modal-open");
 });
