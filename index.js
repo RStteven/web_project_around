a8dbd339d90350b203d3eb340d5cfde7c543e877
Steven <ronaldsbs3@gmail.com>
2025-03-04 22:15:05 +0100

    correcciones entrega

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 let profileModal = document.querySelector(".profile__modal");
-/*let profileModalContent = document.querySelector(".profile__modal-content");*/
-
 let profileModalForm = document.querySelector(".profile__modal-form");
 let profileModalCerrar = document.querySelector(".profile__modal-cerrar");
 let profileInfoButton = document.querySelector(".profile__info-button");
 let profileModalOpen = document.querySelector(".profile__modal-open");
 let profileModalInput = document.querySelectorAll(".profile__modal-input");
-let profileInputName = document.querySelector(".profile__input-name");
 let profileName = document.querySelector(".profile__name");
 let profileText = document.querySelector(".profile__text");
-let profileGuardarButton = document.querySelector(".profile__guardar-button");
 
 function handleOpenModal() {
   profileModal.classList.add("profile__modal-open");
@@ -24,7 +20,6 @@ profileModalCerrar.addEventListener("click", handleCloseModal);
 
 profileModalForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  console.log(profileModalInput);
   profileName.textContent = profileModalInput[0].value;
   profileText.textContent = profileModalInput[1].value;
   profileModal.classList.remove("profile__modal-open");
